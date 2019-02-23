f3c0126de17ece7745e1deb6a7daf6810aa0871c
VladislavHirsa <nazar2207@gmail.com>
2019-02-23 11:43:50 +0200

    initial

--- build/js/all.js
+++ build/js/all.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".header__hat-mobile__menu-hamburger__icon").click(function(){$(this).toggleClass("open")})});console.log(52);
+$(document).ready(function(){$(".header__hat-mobile__menu-hamburger__icon").click(function(){$(this).toggleClass("open"),$(".header__hat-mobile__menu-hamburger__appeare").toggle("slow",function(){$("this").css("display","block")})})});console.log(52);
--- src/js/burger.js
+++ src/js/burger.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('.header__hat-mobile__menu-hamburger__icon').click(function(){
-        $(this).toggleClass('open');
+	$(this).toggleClass('open');
+	$('.header__hat-mobile__menu-hamburger__appeare').toggle('slow', function(){$('this').css('display','block')});
 	});
+
 });
+
